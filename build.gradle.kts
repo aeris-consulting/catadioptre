c287c629a1d78c340f456d2712a49a99718ddc53
Eric Jessé <me@ericjesse.com>
2025-11-10 11:08:50 +0100

    Uses JReleaser to publish to Maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,23 +12,24 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-import com.vanniktech.maven.publish.SonatypeHost
 import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
 import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
 import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
 import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
 import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT
 import org.gradle.api.tasks.testing.logging.TestLogEvent.STARTED
+import org.jetbrains.kotlin.gradle.plugin.extraProperties
+import org.jreleaser.model.Active
+import org.jreleaser.model.Signing
+import org.jreleaser.model.api.deploy.maven.MavenCentralMavenDeployer
+
 
 plugins {
     java
     kotlin("jvm") version "2.0.0" apply false
     kotlin("kapt") version "2.0.0" apply false
-    // Replaces the standard maven publish in order to create a single staging repositories for all the publications.
-    // This avoids staging repositories to be split by heuristic logic from Sonatype.
-    id("com.vanniktech.maven.publish") version "0.31.0"
-    signing
-
+    `maven-publish`
+    id("org.jreleaser") version "1.18.0"
     id("org.sonarqube") version "6.0.+"
 }
 
@@ -46,46 +47,60 @@ allprojects {
 
 subprojects {
     apply(plugin = "java")
-    apply(plugin = "com.vanniktech.maven.publish")
-    apply(plugin = "signing")
+    apply(plugin = "maven-publish")
 
     repositories {
         mavenLocal()
         mavenCentral()
     }
 
+    publishing {
+        repositories {
+            maven {
+                // Local repository to store the artifacts before they are released by JReleaser.
+                name = "PreRelease"
+                setUrl(rootProject.layout.buildDirectory.dir("staging-deploy"))
+            }
+        }
+    }
+
     java {
         sourceCompatibility = target
         targetCompatibility = target
+        withJavadocJar()
+        withSourcesJar()
     }
     val project = this
 
     afterEvaluate {
-        mavenPublishing {
-            pom {
-                name.set(project.name)
-                description.set(project.description)
+        publishing {
+            publications {
+                create<MavenPublication>("maven") {
+                    from(components["java"])
+                    pom {
+                        name.set(project.name)
+                        description.set(project.description)
 
-                url.set("https://catadioptre.aeris-consulting.io")
-                licenses {
-                    license {
-                        name.set("Apache License, Version 2.0 (Apache-2.0)")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                        url.set("https://catadioptre.aeris-consulting.io")
+                        licenses {
+                            license {
+                                name.set("Apache License, Version 2.0 (Apache-2.0)")
+                                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                            }
+                        }
+                        developers {
+                            developer {
+                                id.set("ericjesse")
+                                name.set("Eric Jessé")
+                            }
+                        }
+                        scm {
+                            connection.set("scm:git:https://github.com/aeris-consulting/catadioptre")
+                            url.set("https://github.com/aeris-consulting/catadioptre")
+                        }
                     }
-                }
-                developers {
-                    developer {
-                        id.set("ericjesse")
-                        name.set("Eric Jessé")
-                    }
-                }
-                scm {
-                    url.set("https://github.com/aeris-consulting/catadioptre")
                 }
             }
-
-            publishToMavenCentral(SonatypeHost.S01)
-            signAllPublications()
         }
     }
 
@@ -117,6 +132,44 @@ subprojects {
                 info {
                     events(FAILED, SKIPPED, PASSED, STANDARD_ERROR)
                     exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+                }
+            }
+        }
+    }
+}
+
+jreleaser {
+    gitRootSearch.set(true)
+
+    release {
+        // One least one enabled release provider is mandatory, so let's use Github and disable
+        // all the options.
+        github {
+            skipRelease.set(true)
+            skipTag.set(true)
+            uploadAssets.set(Active.NEVER)
+            token.set("dummy")
+        }
+    }
+
+    val enableSign = !extraProperties.has("aeris.sign") || extraProperties.get("aeris.sign") != "false"
+    if (enableSign) {
+        signing {
+            active.set(Active.ALWAYS)
+            mode.set(Signing.Mode.MEMORY)
+            armored = true
+        }
+    }
+
+    deploy {
+        maven {
+            mavenCentral {
+                register("aeris-releases") {
+                    active.set(Active.RELEASE)
+                    namespace.set("io.aeris-consulting")
+                    applyMavenCentralRules.set(true)
+                    stage.set(MavenCentralMavenDeployer.Stage.UPLOAD)
+                    stagingRepository(layout.buildDirectory.dir("staging-deploy").get().asFile.path)
                 }
             }
         }
